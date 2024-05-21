30d5e5b3646f493c2046b25b9ec9dfd7d46cc5c4
Rasmus Lindblom <rasmus.lindblom.04@hotmail.com>
2024-05-21 16:51:21 +0200

    big bang

--- Get_Recept_Info.py
+++ Get_Recept_Info.py
@@ -4,39 +4,73 @@ from bs4 import BeautifulSoup
 def get_recept_info(link):
     r=requests.get(f'https://www.mathem.se{link}')
     soup = BeautifulSoup(r.text, 'html.parser')
+    
+    title = get_title(soup)
+    img = get_image(soup)
+    description = get_description(soup)
+    ingredients = get_ingrediens(soup)
+    instructions = get_instructions(soup)
+    time = get_time(soup)
 
-    img = soup.find_all('img', class_="k-image--cover")[12]['src']
-    
-    text = soup.find_all('span', class_="k-text-style--body-m")[24].get_text(separator='')
-    
+    return (title, img, description, ingredients, instructions, time)
+
+def get_title(soup):
+    return soup.h1.text
+
+def get_image(soup):
+    return soup.find_all('img', class_="k-image--cover")[12]['src']
+
+def get_description(soup):
+    description = soup.find_all('span', class_="k-text-style--body-m")[24].text.split('\n')
+    return description[0]
+
+def get_ingrediens(soup):
     ingredients_html = soup.find_all('table')
     ingredients_html = BeautifulSoup(f'<html><body>{ingredients_html[0]}</html></body>', "html.parser").find_all('tr')
     ingredients_list = []
-    
+
     index = -1
     for ingredient in ingredients_html:
-        ingredient = ingredient.text
-        
-        if ingredient[0].isalpha():
+        ingredient = ingredient.text.strip()
+
+        if ingredient=='':
             index+=1
             ingredients_list.append([])
+            ingredients_list[index].append('Ingredienser')
 
-        ingredients_list[index].append(ingredient)
+        elif ingredient[0].isalpha():
+            index+=1
+            ingredients_list.append([])
+            ingredients_list[index].append(ingredient)
 
-    instructions = soup.find_all('p')
+        else:
+            ing = ""
+            flag=False
+            for letter in ingredient:
+                if not letter.isupper() or flag:
+                    ing += letter
+                elif not flag:
+                    flag = True
+                    ing += f' {letter}'
+            ingredients_list[index].append(ing)
+   
+    return tuple(ingredients_list)
+
+def get_instructions(soup):
+    instructions = soup.find_all('p', class_=None)
     instructions_list = []
 
-    for instruction in instructions[3:-1]:
+    for instruction in instructions:
         instruction=instruction.text
 
-        if instruction == 'Bild: ':
+        if instruction[0:13] == 'Näringsvärde:' or instruction == 'Bild: ' or instruction[0:8] == 'Styling:':
             break
 
         instructions_list.append(instruction)
-    print(instructions_list)
-        
 
+    return tuple(instructions_list)
 
+def get_time(soup):
+    return soup.find_all('span', class_='k-text-style--label-xs')[30].text
 
-get_recept_info('/se/recipes/248-mari-bergman-gronsakspasta-med-artpesto-och-pumpakarnor/?portions=4')
-# get_recept_info('/se/recipes/241-mari-bergman-fishn-chips-med-appelrora/?portions=4')
+get_recept_info('/se/recipes/3582-mari-bergman-omelett-med-ost-och-kalkon/')
--- main.py
+++ main.py
@@ -1,3 +1,53 @@
 from Get_Sub_Sites import get_sub_sites
+from Get_Recept_Info import get_recept_info
+from sqlalchemy import create_engine, Column, Integer, String, MetaData, Table, BLOB, insert
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
 
-links = get_sub_sites()
+Base = declarative_base()
+
+class Recipe(Base):
+    __tablename__ = 'recipe',
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100))
+    ingredients = Column(String(1000))
+    instrucrions = Column(String(1000))
+    image_name = Column(String(100))
+    time = Column(String(100))
+    description = Column(String(1000))
+
+if __name__ == '__main__':
+    links = get_sub_sites()
+
+    for link in links:
+        print(link)
+        title, img, description, ingredients, instructions, time = get_recept_info(link)
+        engine = create_engine('sqlite:///database.db')
+        Base.metadata.create_all(engine)
+
+
+        engine = create_engine('sqlite:///database.db', echo=False)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        engine = create_engine('sqlite:///database.db', echo=False)
+        stmt = insert(Recipe).values(
+            title = title,
+            ingredients = str(ingredients),
+            instrucrions = str(instructions),
+            image_name = img,
+            time = time,
+            description = description
+        )
+
+        with engine.connect() as conn:
+            conn.execute(stmt)
+            conn.commit()
+
+
+
+
+
+
